d35d1901e8ddcb059ce0a632d5ed29772633c6fb
nglbrrts <angelobarrientos1@gmail.com>
2024-06-26 10:33:43 -0400

    ADD: ajustes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-
+import CompanyHeader from "../components/companyHeader";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 background-slate-50">
+      <CompanyHeader />
     </main>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+// tailwind.config.js
 
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Adjust the paths according to your project's file structure
+    './public/index.html', // Add any other files that use Tailwind CSS classes
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#1DA1F2',
+        secondary: '#14171A',
+        accent: '#657786',
+        background: '#F5F8FA',
+      },
+      spacing: {
+        '72': '18rem',
+        '84': '21rem',
+        '96': '24rem',
+      },
+    },
+  },
+  plugins: [
+  ],
+};
